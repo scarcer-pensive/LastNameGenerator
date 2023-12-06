ff17cbce1218cb910b5fad43e38559a25693c21d
Pierre Dubois <152968677+scarcer-pensive@users.noreply.github.com>
2023-12-06 19:49:39 +0700

    Create NameApp.java

---
+++ NameApp.java
@@ -0,0 +1,6 @@
+public class NameApp {
+    public static void main(String[] args) {
+        String generatedLastName = LastNameGenerator.generateLastName();
+        System.out.println("Generated Last Name: " + generatedLastName);
+    }
+}
